55cb2ef4d897800eb7fd06a24079da2ecdb8d294
Anshu kumar <40786793+aanshu-kumar@users.noreply.github.com>
2025-01-27 12:21:17 +0530

    Update Card.js

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,1 @@
-// Card.js
-import React from "react";
 
-const Card = ({ children }) => {
-  return (
-    <div className="p-6 bg-white rounded-2xl shadow-lg max-w-md mx-auto">
-      {children}
-    </div>
-  );
-};
-
-export default Card;
